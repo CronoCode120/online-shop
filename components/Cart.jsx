569eed59484eb089a2262099a277b1dda80b9977
CronoCode120 <131760898+CronoCode120@users.noreply.github.com>
2023-04-25 18:51:45 +0200

    Update Cart.jsx

--- components/Cart.jsx
+++ components/Cart.jsx
@@ -71,7 +71,7 @@ const Cart = () => {
                     <div className='cart-item-details'>
                       <p>{item.name}</p>
                       {item.discount ? (
-                        <p className='discount-container' style={{'fontWeight':'600', 'fontFamily':'"Poppins", sans-serif', 'fontSize':'16px'}}><span className='prev-price'>${item.price.toFixed(2)}</span><span className='discount-price'>${calculateDiscount(item.price, item.discount)}</span></p>
+                        <p className='discount-container' style={{'fontWeight':'600', 'fontFamily':'"Poppins", sans-serif', 'fontSize':'16px', 'justifyContent'='flex-end !important'}}><span className='prev-price'>${item.price.toFixed(2)}</span><span className='discount-price'>${calculateDiscount(item.price, item.discount)}</span></p>
                       ) : (
                         <p style={{'fontWeight':'600', 'fontFamily':'"Poppins", sans-serif', 'fontSize':'16px'}}>${item.price}</p>
                       )}
